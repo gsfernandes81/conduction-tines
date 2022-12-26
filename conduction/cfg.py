28aa33629c2e9c9ea1f01faa70564ba90d4edcd8
gsfernandes81 <gsfernandes81@gmail.com>
2022-12-26 22:26:58 +0000

    Set up base poetry project

---
+++ conduction/cfg.py
@@ -0,0 +1,19 @@
+# Copyright © 2019-present gsfernandes81
+
+# This file is part of "conduction-tines".
+
+# conduction-tines is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+
+# "conduction-tines" is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along with
+# conduction-tines. If not, see <https://www.gnu.org/licenses/>.
+
+from os import getenv as _getenv
+
+# Discord API Token
+discord_token = _getenv("DISCORD_TOKEN")
