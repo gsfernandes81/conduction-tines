d5deb0ded687fd9373dabccd7ae323b6a75f1424
gsfernandes81 <gsfernandes81@gmail.com>
2023-03-16 18:43:09 +0000

    Add retry logic and error loggin to repeater.py

--- conduction/modules/repeater.py
+++ conduction/modules/repeater.py
@@ -15,91 +15,139 @@
 
 import json
 import logging
+from asyncio import sleep, TimeoutError
 
 import hikari as h
 import lightbulb as lb
 
-from ..schemas import MirroredChannel, MirroredMessage, db_session
+from .. import utils
 from ..bot import CachedFetchBot
+from ..schemas import MirroredChannel, MirroredMessage, db_session
 
 
 async def message_create_repeater(event: h.MessageCreateEvent):
     msg = event.message
     bot = event.app
 
-    mirrors = await MirroredChannel.get_or_fetch_dests(msg.channel_id)
-    if not mirrors:
-        # Return if this channel is not to be mirrored
-        # ie if no mirror list found for it
-        return
+    backoff_timer = 30
+    while True:
+        try:
+            mirrors = await MirroredChannel.get_or_fetch_dests(msg.channel_id)
+            if not mirrors:
+                # Return if this channel is not to be mirrored
+                # ie if no mirror list found for it
+                return
 
-    logging.info(f"MessageCreateEvent received for messge in <#{msg.channel_id}>")
+            logging.info(
+                f"MessageCreateEvent received for messge in <#{msg.channel_id}>"
+            )
 
-    if not h.MessageFlag.CROSSPOSTED in msg.flags:
-        logging.info(f"Message in <#{msg.channel_id}> not crossposted, waiting...")
-        await bot.wait_for(
-            h.MessageUpdateEvent,
-            timeout=12 * 60 * 60,
-            predicate=lambda e: e.message.id == msg.id
-            and e.message.flags
-            and h.MessageFlag.CROSSPOSTED in e.message.flags,
-        )
-        logging.info(
-            f"Crosspost event received for messge in in <#{msg.channel_id}>, "
-            + "continuing..."
-        )
+            if not h.MessageFlag.CROSSPOSTED in msg.flags:
+                logging.info(
+                    f"Message in <#{msg.channel_id}> not crossposted, waiting..."
+                )
+                await bot.wait_for(
+                    h.MessageUpdateEvent,
+                    timeout=12 * 60 * 60,
+                    predicate=lambda e: e.message.id == msg.id
+                    and e.message.flags
+                    and h.MessageFlag.CROSSPOSTED in e.message.flags,
+                )
+                logging.info(
+                    f"Crosspost event received for messge in in <#{msg.channel_id}>, "
+                    + "continuing..."
+                )
+        except TimeoutError:
+            break
+        except Exception as e:
+            utils.discord_error_logger(bot, e)
+            await sleep(backoff_timer)
+            backoff_timer += 30 / backoff_timer
+        else:
+            break
 
-    for mirror_ch_id in mirrors:
-        channel: h.TextableChannel = await bot.fetch_channel(mirror_ch_id)
-
-        if not isinstance(channel, h.TextableChannel):
-            # Ignore non textable channels
-            continue
-
-        async with db_session() as session:
+    async with db_session() as session:
+        for mirror_ch_id in mirrors:
             async with session.begin():
-                try:
-                    # Send the message
-                    mirrored_msg = await channel.send(
-                        msg.content,
-                        attachments=msg.attachments,
-                        components=msg.components,
-                        embeds=msg.embeds,
+                for _ in range(3):  # Retry 3 times at most
+                    try:
+                        channel: h.TextableChannel = await bot.fetch_channel(
+                            mirror_ch_id
+                        )
+
+                        if not isinstance(channel, h.TextableChannel):
+                            # Ignore non textable channels
+                            continue
+
+                        # Send the message
+                        mirrored_msg = await channel.send(
+                            msg.content,
+                            attachments=msg.attachments,
+                            components=msg.components,
+                            embeds=msg.embeds,
+                        )
+                    except Exception as e:
+                        logging.error(
+                            f"Retrying message send in {mirror_ch_id} due to error:"
+                        )
+                        utils.discord_error_logger(bot, e)
+                        continue
+                    else:
+                        break
+                else:
+                    logging.error(
+                        "Failed to send message in {mirror_ch_id} after 3 tries"
                     )
-                    # Record the ids in the db
-                    await MirroredMessage.add_msg(
-                        dest_msg=mirrored_msg.id,
-                        dest_channel=mirrored_msg.channel_id,
-                        source_msg=msg.id,
-                        source_channel=event.channel_id,
-                        session=session,
-                    )
-                except Exception as e:
-                    logging.exception(e)
+                    break
+                # Record the ids in the db
+                await MirroredMessage.add_msg(
+                    dest_msg=mirrored_msg.id,
+                    dest_channel=mirrored_msg.channel_id,
+                    source_msg=msg.id,
+                    source_channel=event.channel_id,
+                    session=session,
+                )
 
 
 async def message_update_repeater(event: h.MessageUpdateEvent):
     msg = event.message
     bot = event.app
 
-    if not await MirroredChannel.get_or_fetch_dests(msg.channel_id):
-        # Return if this channel is not to be mirrored
-        # ie if no mirror list found for it
-        return
+    backoff_timer = 30
+    while True:
+        try:
+            if not await MirroredChannel.get_or_fetch_dests(msg.channel_id):
+                # Return if this channel is not to be mirrored
+                # ie if no mirror list found for it
+                return
 
-    msgs_to_update = await MirroredMessage.get_dest_msgs_and_channels(msg.id)
+            msgs_to_update = await MirroredMessage.get_dest_msgs_and_channels(msg.id)
+
+        except Exception as e:
+            utils.discord_error_logger(bot, e)
+            await sleep(backoff_timer)
+            backoff_timer += 30 / backoff_timer
+        else:
+            break
 
     for msg_id, channel_id in msgs_to_update:
-        try:
-            dest_msg = await bot.fetch_message(channel_id, msg_id)
-            await dest_msg.edit(
-                msg.content,
-                attachments=msg.attachments,
-                components=msg.components,
-                embeds=msg.embeds,
+        for _ in range(3):  # Retry 3 times at most
+            try:
+                dest_msg = await bot.fetch_message(channel_id, msg_id)
+                await dest_msg.edit(
+                    msg.content,
+                    attachments=msg.attachments,
+                    components=msg.components,
+                    embeds=msg.embeds,
+                )
+            except Exception as e:
+                utils.discord_error_logger(bot, e)
+            else:
+                break
+        else:
+            logging.error(
+                f"Failed to update message {msg_id} in {channel_id} after 3 tries"
             )
-        except Exception as e:
-            logging.exception(e)
 
 
 @lb.command(name="repeater", description="Repeater control commands")
