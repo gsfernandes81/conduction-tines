e093dea8133819cd7fa58235fa66855eed267348
gsfernandes81 <gsfernandes81@gmail.com>
2023-03-16 15:52:46 +0000

    Move eververse to NavPages system

--- conduction/modules/eververse.py
+++ conduction/modules/eververse.py
@@ -13,69 +13,66 @@
 # You should have received a copy of the GNU Affero General Public License along with
 # conduction-tines. If not, see <https://www.gnu.org/licenses/>.
 
+import datetime as dt
+import typing as t
+
 import hikari as h
 import lightbulb as lb
-from lightbulb.ext import tasks
 
 from .. import cfg, utils
-from ..bot import CachedFetchBot
-from . import autocmd
+from .autocmd import MessagePrototype, NavigatorView, NavPages
 from .autoposts import autopost_command_group, follow_control_command_maker
 
 EVERVERSE_WEEKLY = cfg.followables["eververse"]
 EVERVERSE_DAILY = cfg.followables["daily_reset"]
 
 
-@tasks.task(m=1, auto_start=True, wait_before_execution=False, pass_app=True)
-async def refresh_eververse_weekly_data(bot: CachedFetchBot) -> None:
-    global eververse_weekly_message_kwargs
+class EVWeeklyPages(NavPages):
+    @classmethod
+    def preprocess_messages(
+        cls, messages: t.List[MessagePrototype | h.Message]
+    ) -> MessagePrototype:
 
-    messages = await autocmd.pull_messages_from_channel(
-        bot, after=utils.weekly_reset_period()[0], channel_id=EVERVERSE_WEEKLY
+        processed_messages = [
+            MessagePrototype.from_message(m)
+            .merge_content_into_embed(0)
+            .merge_attachements_into_embed(designator=n)
+            for n, m in enumerate(messages)
+        ]
+        return utils.accumulate(processed_messages)
+
+    @classmethod
+    def period_around(cls, date: dt.datetime = None) -> t.Tuple[dt.datetime]:
+        return utils.weekly_reset_period(date)
+
+
+class EVDailyPages(NavPages):
+    @classmethod
+    def preprocess_messages(
+        cls, messages: t.List[MessagePrototype | h.Message]
+    ) -> MessagePrototype:
+        processed_messages = [
+            MessagePrototype.from_message(m)
+            .merge_content_into_embed(0)
+            .merge_attachements_into_embed(designator=n)
+            for n, m in enumerate(messages)
+        ]
+        return utils.accumulate(processed_messages)
+
+    @classmethod
+    def period_around(cls, date: dt.datetime = None) -> t.Tuple[dt.datetime]:
+        return utils.daily_reset_period(date)
+
+
+async def on_start(event: h.StartedEvent):
+    global evweekly
+    evweekly = await EVWeeklyPages.from_channel(
+        event.app, EVERVERSE_WEEKLY, history_len=4
     )
-
-    # Merge all the images into the last embed for each message
-    # and all the content into the first embed for each message
-    msg_protos: autocmd.MessagePrototype = []
-    try:
-        for message_no, message in enumerate(messages):
-            # Merge content into the first embed if it exists
-            # else make a new embed and do the same
-            msg_proto = autocmd.MessagePrototype.from_message(message)
-            msg_proto.merge_content_into_embed(0)
-            msg_proto.merge_attachements_into_embed(designator=message_no)
-            msg_protos.append(msg_proto)
-    except Exception as e:
-        utils.discord_error_logger(bot, e)
-        raise e
-
-    eververse_weekly_message_kwargs = utils.accumulate(msg_protos).to_message_kwargs()
-
-
-@tasks.task(m=1, auto_start=True, wait_before_execution=False, pass_app=True)
-async def refresh_eververse_daily_data(bot: CachedFetchBot) -> None:
-    global eververse_daily_message_kwargs
-
-    messages = await autocmd.pull_messages_from_channel(
-        bot, after=utils.daily_reset_period()[0], channel_id=EVERVERSE_DAILY
+    global evdaily
+    evdaily = await EVDailyPages.from_channel(
+        event.app, EVERVERSE_DAILY, history_len=14
     )
-
-    # Merge all the images into the last embed for each message
-    # and all the content into the first embed for each message
-    msg_protos: autocmd.MessagePrototype = []
-    try:
-        for message_no, message in enumerate(messages):
-            # Merge content into the first embed if it exists
-            # else make a new embed and do the same
-            msg_proto = autocmd.MessagePrototype.from_message(message)
-            msg_proto.merge_content_into_embed(0)
-            msg_proto.merge_attachements_into_embed(designator=message_no)
-            msg_protos.append(msg_proto)
-    except Exception as e:
-        utils.discord_error_logger(bot, e)
-        raise e
-
-    eververse_daily_message_kwargs = utils.accumulate(msg_protos).to_message_kwargs()
 
 
 @lb.command("eververse", "Find out about the eververse items")
@@ -88,23 +85,21 @@ async def eververse_group(ctx: lb.Context):
 @lb.command("weekly", "Find out about this weeks eververse items")
 @lb.implements(lb.SlashSubCommand)
 async def eververse_weekly(ctx: lb.Context):
-    await ctx.respond(h.ResponseType.DEFERRED_MESSAGE_CREATE)
-    await ctx.respond(**eververse_weekly_message_kwargs)
+    navigator = NavigatorView(pages=evweekly, timeout=60, autodefer=True)
+    await navigator.send(ctx.interaction)
 
 
 @eververse_group.child
 @lb.command("daily", "Find out about today's eververse offer")
 @lb.implements(lb.SlashSubCommand)
 async def eververse_daily(ctx: lb.Context):
-    await ctx.respond(h.ResponseType.DEFERRED_MESSAGE_CREATE)
-    await ctx.respond(**eververse_daily_message_kwargs)
+    navigator = NavigatorView(pages=evdaily, timeout=60, autodefer=True)
+    await navigator.send(ctx.interaction)
 
 
 def register(bot):
-    for command in [
-        eververse_group,
-    ]:
-        bot.command(command)
+    bot.command(eververse_group),
+    bot.listen()(on_start)
 
     autopost_command_group.child(
         follow_control_command_maker(
