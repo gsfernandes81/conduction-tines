fbd9e6d61a10379848a62b7e20cd10a0d6d807c4
gsfernandes81 <gsfernandes81@gmail.com>
2023-08-02 07:41:16 +0100

    Switch ada to single page mode

--- conduction/modules/ada.py
+++ conduction/modules/ada.py
@@ -14,19 +14,19 @@
 # conduction-tines. If not, see <https://www.gnu.org/licenses/>.
 
 import datetime as dt
-import typing as t
 
 import hikari as h
 import lightbulb as lb
-from hmessage import HMessage as MessagePrototype
 
-from .. import cfg, utils
-from ..nav import NavigatorView, NavPages
+from .. import cfg
+from ..nav import NavigatorView, NavPages, NO_DATA_HERE_EMBED
 from .autoposts import autopost_command_group, follow_control_command_maker
 
 REFERENCE_DATE = dt.datetime(2023, 7, 14, 17, tzinfo=dt.timezone.utc)
 
 FOLLOWABLE_CHANNEL = cfg.followables["ada"]
+
+SINGLE_PAGE_MODE = True
 
 
 async def on_start(event: h.StartedEvent):
@@ -41,11 +41,34 @@ async def on_start(event: h.StartedEvent):
     )
 
 
-@lb.command("ada", "Find out about ada's weekly items")
-@lb.implements(lb.SlashCommand)
-async def ada_command(ctx: lb.Context):
-    navigator = NavigatorView(pages=ada_pages, timeout=60)
-    await navigator.send(ctx.interaction)
+if SINGLE_PAGE_MODE:
+
+    @lb.command("ada", "Find out about ada's weekly items", auto_defer=True)
+    @lb.implements(lb.SlashCommand)
+    async def ada_command(ctx: lb.Context):
+        page_no = 0
+        while True:
+            try:
+                page = ada_pages[page_no]
+            except IndexError:
+                await ctx.respond(NO_DATA_HERE_EMBED)
+            except:
+                page_no -= 1
+                continue
+
+            if page.embeds and page.embeds[0] == NO_DATA_HERE_EMBED:
+                page_no -= 1
+                continue
+            else:
+                await ctx.respond(**page.to_message_kwargs())
+
+else:
+
+    @lb.command("ada", "Find out about ada's weekly items")
+    @lb.implements(lb.SlashCommand)
+    async def ada_command(ctx: lb.Context):
+        navigator = NavigatorView(pages=ada_pages, timeout=60)
+        await navigator.send(ctx.interaction)
 
 
 def register(bot):
